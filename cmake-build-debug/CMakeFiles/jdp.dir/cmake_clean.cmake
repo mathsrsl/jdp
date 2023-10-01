2b1438dfd67bb79a9a1e767c9d17e5156a0fda1c
mathsrsl <101506457+mathsrsl@users.noreply.github.com>
2023-10-01 12:19:31 +0200

    .

---
+++ cmake-build-debug/CMakeFiles/jdp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jdp.dir/main.c.obj"
+  "CMakeFiles/jdp.dir/main.c.obj.d"
+  "jdp.exe"
+  "jdp.exe.manifest"
+  "jdp.pdb"
+  "libjdp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jdp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/rm306044/Documents/GitHub/jdp
+# Install script for directory: F:/users/maths/Documents/GitHub/jdp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/rm306044/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/maths/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/rm306044/Documents/GitHub/jdp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/users/maths/Documents/GitHub/jdp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
